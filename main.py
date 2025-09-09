849995f7dfe50ba2c68dad9129ac1c2d247521a3
domyself1 <2470065375@qq.com>
2025-09-09 18:05:00 +0800

    完成本次作业

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import math   #运用math第三方包解决四舍五入问题
-WeightStr = input("请输入带单位的重量值: ").strip()
-if WeightStr[-2:] == "kg": 
-    value = float(WeightStr[0:-2])
+Weight = input("请输入带单位的重量值: ")
+if Weight[-2:] == "kg":   # 输入以 kg 结尾
+    value = float(Weight[0:-2])
     pd = value * 2.2046
-    pd = math.floor(pd * 1000) / 1000  # 保留三位小数，向下取整
+    pd = int(pd * 1000) / 1000   # 截断保留三位小数
     print("对应的英制重量为{:.3f}磅".format(pd))
-elif WeightStr[-2:] == "pd":
-    value = float(WeightStr[0:-2])
+elif Weight[-2:] == "pd": # 输入以 pd 结尾
+    value = float(Weight[0:-2])
     kg = value / 2.2046
-    kg = math.floor(kg * 1000) / 1000  # 保留三位小数，向下取整
+    kg = int(kg * 1000) / 1000   # 截断保留三位小数
     print("对应的公制重量为{:.3f}公斤".format(kg))
 else:
     print("输入格式错误，请输入类似 '10kg' 或 '10pd'")
