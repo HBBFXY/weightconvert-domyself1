9f84411be52a8c93df8e535089beb1085a53db06
domyself1 <2470065375@qq.com>
2025-09-09 18:07:13 +0800

    完成本次作业

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-Weight = input("请输入带单位的重量值: ")
-if Weight[-2:] == "kg":   # 输入以 kg 结尾
-    value = float(Weight[0:-2])
-    pd = value * 2.2046
-    pd = int(pd * 1000) / 1000   # 截断保留三位小数
-    print("对应的英制重量为{:.3f}磅".format(pd))
-elif Weight[-2:] == "pd": # 输入以 pd 结尾
-    value = float(Weight[0:-2])
-    kg = value / 2.2046
-    kg = int(kg * 1000) / 1000   # 截断保留三位小数
-    print("对应的公制重量为{:.3f}公斤".format(kg))
+weight_str = input()
+unit = weight_str[-2:]
+value = float(weight_str[:-2])
+if unit == "kg":
+    pounds = value * 2.2046
+    print(f"对应的英制重量为{pounds:.3f}磅")
+elif unit == "pd":
+    kilograms = value * 0.4535
+    print(f"对应的公制重量为{kilograms:.3f}公斤")
 else:
-    print("输入格式错误，请输入类似 '10kg' 或 '10pd'")
+    print("输入格式错误")
